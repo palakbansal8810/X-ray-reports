a6fb401cc3c6e216a74368e6f097dd665a5d5f04
Kartik1260 <131158199+Kartik1260@users.noreply.github.com>
2024-09-08 16:36:40 +0530

    Port changed

--- main.py
+++ main.py
@@ -94,4 +94,5 @@ def predict_tumor():
 if __name__ == "__main__":
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
